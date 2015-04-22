419802a03182c1e3dce31d8c9b410d293c76abf8
inderpreet99 <inderpreet99@gmail.com>
2015-04-21 23:11:16 -0400

    bump version 0.1-BU2

--- key-auth.php
+++ key-auth.php
@@ -4,7 +4,7 @@
  * Description: API/Secret Key Authentication handler for the JSON API
  * Author: Paul Hughes and WP API Team
  * Author URI: https://github.com/WP-API
- * Version: 0.1-BU1
+ * Version: 0.1-BU2
  * Plugin URI: https://github.com/WP-API/Key-Auth
  */
 
